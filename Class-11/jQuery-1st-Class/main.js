1c88c1a46b2ab94506247a0a7d9678690609b8c3
Monmoy042 <monmoykms.16@gmail.com>
2019-05-18 11:26:56 +0600

    new jQuery Added

--- Class-11/jQuery-1st-Class/main.js
+++ Class-11/jQuery-1st-Class/main.js
@@ -20,13 +20,54 @@ $('#btn3').dblclick(function(){
 });
 
 // Append | Prepend | Before | After
+// $('#Append').click(function(){
+//     $('#text2').append('<b>Bangladesh</b>');
+// });
+
 $('#Append').click(function(){
-    $('#text2').append('<b>Bangladesh</b>');
+	$('#text2').append('<img src="spiderMan.jpg" id="image">');
+});
+// $('#Prepend').click(function(){
+//     $('#text2').prepend('<b>Bangladesh</b>');
+// });
+$('#Prepend').dblclick(function(){
+	$('#text2').prepend('<img src="spiderMan.jpg" id="image">');
 });
 
-$('#Prepend').click(function(){
-    $('#text2').prepend('<b>Bangladesh</b>');
+$('#Before').click(function(){
+	$('#text2').before('<img src="spiderMan.jpg" id="image">');
 });
+
+$('#After').click(function(){
+	$('#text2').after('<img src="spiderMan.jpg" id="image">');
+});
+
+// Slide Up | Slide Down | Slide Toggle
+$('#slideUp').click(function(){
+	$('#text3').slideUp(5000);
+});
+
+$('#slideDown').click(function(){
+	$('#text3').slideDown('fast');
+});
+
+$('#slideToggle').click(function(){
+	$('#text3').slideToggle('slow');
+});
+
+// Fade In | Fade Out | Fade Toggle
+$('#fadeIn').click(function(){
+	$('#text4').fadeIn(5000);
+});
+
+// Scroll to top
+$('#top').click(function(){
+	$('html,body').animate({
+		scrollTop: 0
+	},5000);
+});
+
+
 
 });
 
